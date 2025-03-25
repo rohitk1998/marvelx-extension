591f2c03ce0685d0db360b14d84bd2e97379552b
Rohit Kumar <rohit.kumar@antiersolutions.com>
2025-03-25 11:16:36 +0530

    add right url

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -72,7 +72,7 @@ const ROUTES = {
   EDIT_ACCOUNT:'/edit-account'
 };
 
-const BASE_URL='https://4209-112-196-81-250.ngrok-free.app';
+const BASE_URL='https://walletback-mnlv.onrender.com';
 
 const API_URL={
   createWallet:`${BASE_URL}/user/create-wallet`,
